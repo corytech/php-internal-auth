0a020fad548b7e8f3d28c04b88f5eaa2c1e8baff
Dmytro Pechura <dmytro.pechura@crosspay.net>
2026-07-22 13:12:05 +0300

    fix: add to service

--- src/Resources/config/services.php
+++ src/Resources/config/services.php
@@ -5,6 +5,7 @@ declare(strict_types=1);
 namespace Symfony\Component\DependencyInjection\Loader\Configurator;
 
 use Corytech\InternalAuth\InternalApiAuthenticator;
+use Corytech\InternalAuth\InternalApiService;
 
 return static function (ContainerConfigurator $container) {
     $container->services()
@@ -12,5 +13,6 @@ return static function (ContainerConfigurator $container) {
         ->autoconfigure()
         ->autowire()
         ->set(InternalApiAuthenticator::class, InternalApiAuthenticator::class)
+        ->set(InternalApiService::class, InternalApiService::class)
     ;
 };
